f04c7b3a668b9fe3b00c31e2a792ad71c53ab0a4
amusulux <105339202+amusulux@users.noreply.github.com>
2023-03-27 18:58:15 +0530

    Add files via upload

---
+++ build_image.py
@@ -0,0 +1,10 @@
+import os, time
+
+images_before = os.popen("sudo docker images").read().strip().split("\n")[1:]
+print(images_before)
+os.popen("sudo docker build -t ubuntu:v1 /var/lib/jenkins/workspace/Sample_commit_project")
+time.sleep(5)
+images_after = os.popen("sudo docker images").read().strip().split('\n')[1:]
+print(images_after)
+if len(images_before) >= len(images_after):
+    raise Exception("Image creation was unsuccessful")
