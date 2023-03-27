948ce742f3a3291a01eed821563a6361f463f9e1
amusulux <105339202+amusulux@users.noreply.github.com>
2023-03-27 19:01:38 +0530

    Add files via upload

--- build_image.py
+++ build_image.py
@@ -2,7 +2,7 @@ import os, time
 
 images_before = os.popen("sudo docker images").read().strip().split("\n")[1:]
 print(images_before)
-os.popen("sudo docker build -t ubuntu:v1 /var/lib/jenkins/workspace/Sample_commit_project")
+os.popen("sudo docker build -t ubuntu:v1 home/remlab/Desktop/jenkin_agent/workspace/Sample_commit_project")
 time.sleep(5)
 images_after = os.popen("sudo docker images").read().strip().split('\n')[1:]
 print(images_after)
